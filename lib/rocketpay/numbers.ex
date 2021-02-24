79c836b48f01a0de787f528772cdb59b18e38ca6
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-23 22:02:47 -0300

    New: Imutability concept

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -5,6 +5,11 @@ defmodule Rocketpay.Numbers do
     |>handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result = String.split(result, ",")
+    result = Enum.map(result, fn number -> String.to_integer(number) end)
+    result = Enum.sum(result)
+    result
+  end
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 end
