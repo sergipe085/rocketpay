dfbd8d6dba62f704abc7affd1cc7479bcc6b1f98
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-23 21:51:14 -0300

    New: Catching invalid file error

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -1,5 +1,9 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    {:ok, file} = File.read("#{filename}.csv")
+    file = File.read("#{filename}.csv")
+    handle_file(file)
   end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 end
